1e555f02b204696c8cc39bf6237a478f85c9fa7b
sukibaby <163092272+sukibaby@users.noreply.github.com>
2026-01-11 05:44:33 -0800

    docs: shorten help text

--- include/main.hpp
+++ include/main.hpp
@@ -9,7 +9,7 @@ constexpr long default_timeout = PerformanceTester::DefaultTimeoutMs;
 inline std::string getHelpText()
 {
     std::ostringstream Out;
-    Out << R"(  --country <code>              Filter mirrors by country. Uses standard country codes (ISO 3166-1 alpha-2 country codes such as FR, DE, US, CN, etc.)
+    Out << R"(  --country <code>              Filter mirrors by country. Uses ISO 3166-1 alpha-2 country codes (such as FR, DE, US, CN, etc.)
   --no-official-mirrors         Exclude debian.org mirrors
   --only-official-mirrors       Only test debian.org mirrors
   --count <number>              Number of top results to display. Default is 5.
